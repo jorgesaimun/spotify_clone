f7bcab1341cabb6d2ff249a11426a56f4ca993d0
jorgesaimun <saifullahsaimun1505@gmail.com>
2024-12-11 10:43:12 +0600

    complete stack implementation on widgets

--- lib/presentation/auth/pages/signup_or_signin.dart
+++ lib/presentation/auth/pages/signup_or_signin.dart
@@ -10,74 +10,93 @@ class SignupOrSignin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          children: [
-            SvgPicture.asset(AppVectors.logo),
-            const SizedBox(
-              height: 50,
-            ),
-            const Text(
-              "Enjoy Listening To Music",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 24,
-              ),
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            const Text(
-              "Discover new artists, albums, and playlists. "
-              "Create your own personalized music library. "
-              "Join the community of music lovers today!",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16,
-                color: AppColors.grey,
-              ),
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: BasicAppButton(
-                    onPressed: () {
-                      // Navigator.push(
-                      //   context,
-                      //   MaterialPageRoute(
-                      //     builder: (BuildContext context) => const SignupOrSignin(),
-                      //   ),
-                      // );
-                    },
-                    title: 'Register',
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(children: [
+          Align(
+            alignment: Alignment.topRight,
+            child: SvgPicture.asset(AppVectors.topPattern),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: SvgPicture.asset(AppVectors.bottomPattern),
+          ),
+
+          
+          Align(
+            alignment: Alignment.center,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(AppVectors.logo),
+                  const SizedBox(
+                    height: 50,
                   ),
-                ),
-                const SizedBox(
-                  width: 8,
-                ),
-                Expanded(
-                  flex: 1,
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Sign in',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        color: context.isDarkMode ? Colors.white : Colors.black,
-                      ),
+                  const Text(
+                    "Enjoy Listening To Music",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
                     ),
                   ),
-                ),
-              ],
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  const Text(
+                    "Discover new artists, albums, and playlists. "
+                    "Create your own personalized music library. "
+                    "Join the community of music lovers today!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: AppColors.grey,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: BasicAppButton(
+                          onPressed: () {
+                            // Navigator.push(
+                            //   context,
+                            //   MaterialPageRoute(
+                            //     builder: (BuildContext context) => const SignupOrSignin(),
+                            //   ),
+                            // );
+                          },
+                          title: 'Register',
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Sign in',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: context.isDarkMode
+                                  ? Colors.white
+                                  : Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          )
+        ]),
       ),
     );
   }
