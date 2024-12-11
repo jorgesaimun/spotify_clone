027c3142afd25b1c43ff06929c79c4263b61f478
jorgesaimun <saifullahsaimun1505@gmail.com>
2024-12-11 10:30:40 +0600

    solve widget overlap issue in signup_or_signin page

--- lib/presentation/auth/pages/signup_or_signin.dart
+++ lib/presentation/auth/pages/signup_or_signin.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify_clone/common/helpers/is_dark_mode.dart';
 import 'package:spotify_clone/common/widgets/button/basic_app_button.dart';
 import 'package:spotify_clone/core/configs/assets/app_vectors.dart';
 import 'package:spotify_clone/core/configs/theme/app_colors.dart';
@@ -9,55 +10,69 @@ class SignupOrSignin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Stack(
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
           children: [
-            Align(
-              alignment: Alignment.topRight,
-              child: SvgPicture.asset(
-                AppVectors.topPattern,
+            SvgPicture.asset(AppVectors.logo),
+            const SizedBox(
+              height: 50,
+            ),
+            const Text(
+              "Enjoy Listening To Music",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
               ),
             ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: SvgPicture.asset(
-                AppVectors.bottomPattern,
+            const SizedBox(
+              height: 24,
+            ),
+            const Text(
+              "Discover new artists, albums, and playlists. "
+              "Create your own personalized music library. "
+              "Join the community of music lovers today!",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                color: AppColors.grey,
               ),
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
+            const SizedBox(
+              height: 24,
+            ),
+            Row(
               children: [
-                SvgPicture.asset(AppVectors.logo),
-                const SizedBox(
-                  height: 50,
-                ),
-                const Text(
-                  "Enjoy Listening To Music",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
+                Expanded(
+                  child: BasicAppButton(
+                    onPressed: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //     builder: (BuildContext context) => const SignupOrSignin(),
+                      //   ),
+                      // );
+                    },
+                    title: 'Register',
                   ),
                 ),
                 const SizedBox(
-                  height: 30,
+                  width: 8,
                 ),
-                const Text(
-                  "Details about app",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: AppColors.grey,
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Sign in',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: context.isDarkMode ? Colors.white : Colors.black,
+                      ),
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    BasicAppButton(onPressed: () {}, title: 'Register'),
-                  ],
                 ),
               ],
             ),
