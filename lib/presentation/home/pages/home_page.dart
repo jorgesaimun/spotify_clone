cf141e82349c0b4a74d584434485d9972f7f498f
jorgesaimun <saifullahsaimun1505@gmail.com>
2024-12-13 10:53:11 +0600

    add UserEntity and auth_repository; update HomePage layout with BasicAppBar

--- lib/presentation/home/pages/home_page.dart
+++ lib/presentation/home/pages/home_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_clone/common/widgets/appbar/app_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      appBar: BasicAppBar(),
+      body: Column(),
+    );
   }
 }
