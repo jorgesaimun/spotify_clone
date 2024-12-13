4a86bf923a8a01fedf707a37a997b0b0e3305dc4
jorgesaimun <saifullahsaimun1505@gmail.com>
2024-12-13 14:24:47 +0600

    add CreateUserReq model and abstract AuthFirebaseService for authentication

--- lib/data/sources/auth/auth_firebase_service.dart
+++ lib/data/sources/auth/auth_firebase_service.dart
@@ -1,1 +1,19 @@
+abstract class AuthFirebaseService {
+  Future<void> signin();
+  Future<void> signup();
+}
 
+class AuthFirebaseServiceImpl extends AuthFirebaseService{
+  @override
+  Future<void> signin() {
+    // TODO: implement signin
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signup() {
+    // TODO: implement signup
+    throw UnimplementedError();
+  }
+
+}
